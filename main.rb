1272dd30fb02bab2ffb6661a1d191b5f0d9fa2b9
ohbarye <over.rye@gmail.com>
2019-08-30 20:48:01 +0900

    Introduce tokenizer

--- main.rb
+++ main.rb
@@ -1,24 +1,65 @@
-source = ''
-source_index = 0
+@source = ''
+@source_index = 0
 
 Token = Struct.new(:kind, :value)
 
 def get_char
-  return if source_index == source.size
-  char = source[source_index]
-  source_index += 1
+  return if @source_index == @source.size
+  char = @source[@source_index]
+  @source_index += 1
   char
 end
 
 def unget_char
-  source_index -=1
+  @source_index -=1
+end
+
+def read_number(char)
+  number = [char]
+  while true
+    char = get_char
+    break if char.nil?
+
+    if '0' <= char && char <= '9'
+      number << char
+    else
+      unget_char
+      break
+    end
+  end
+
+  number.join
+end
+
+def tokenize
+  tokens = []
+  puts "# Tokens : "
+
+  while true
+    char = get_char
+    break if char.nil?
+    case char
+    when '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+      intliteral = read_number(char)
+      token = Token.new("intliteral", intliteral)
+      tokens << token
+      puts "# '#{token.value}'"
+    else
+      throw StandardError("tokenizer: Invalid char: '#{char}'")
+    end
+  end
+
+  puts
+  tokens
 end
 
 def main
-  source = gets
+  @source = gets
+  tokens = tokenize
+  first_token = tokens.first
   puts "  .global main"
   puts "main:"
-  puts "  movq $#{source}, %rax"
+  puts "  movq $#{first_token.value}, %rax"
   puts "  ret"
 end
 
