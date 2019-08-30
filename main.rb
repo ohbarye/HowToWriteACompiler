7564172db21b4f392d59294db722bdfe80b5019f
ohbarye <over.rye@gmail.com>
2019-08-30 20:24:39 +0900

    Implement get_char, unget_char

--- main.rb
+++ main.rb
@@ -1,3 +1,17 @@
+source = ''
+source_index = 0
+
+def get_char
+  return if source_index == source.size
+  char = source[source_index]
+  source_index += 1
+  char
+end
+
+def unget_char
+  source_index -=1
+end
+
 def main
   source = gets
   puts "  .global main"
